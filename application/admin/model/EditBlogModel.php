176a0e6094d0f5e90aeab11eb8ad38e12cde4275
wenke <184796824@qq.com>
2019-05-17 14:33:37 +0800

    update

--- application/admin/model/EditBlogModel.php
+++ application/admin/model/EditBlogModel.php
@@ -23,14 +23,15 @@ class EditBlogModel extends Model
         $data = Db::table('article')->limit(15)->order('id', 'desc')->select();
         foreach ($data as &$v) {
             $v['category'] = func::changeCategory($v['category']);
-            $v['created'] = date('Y-m-d', strtotime($v['created']));
-            if (strpos($v['content'], '<br>')) {
-                $v['content'] = substr($v['content'], 0, strpos($v['content'], '<br>'));
-            } elseif (strpos($v['content'], '<pre>')) {
-                $v['content'] = substr($v['content'], 0, strpos($v['content'], '<pre>'));
-            } else {
-                $v['content'] = substr($v['content'], 0, 100);
-            }
+//            $v['created'] = date('Y-m-d', strtotime($v['created']));
+//            if (strpos($v['content'], '<br>')) {
+//                $v['content'] = substr($v['content'], 0, strpos($v['content'], '<br>'));
+//            } elseif (strpos($v['content'], '<pre>')) {
+//                $v['content'] = substr($v['content'], 0, strpos($v['content'], '<pre>'));
+//            } else {
+//                $v['content'] = substr($v['content'], 0, 100);
+//            }
+            $v['visitable'] = $v['visitable'] == 0 ? 'false' : 'true';
         }
         return $data;
     }
